fbbd41a23964ef8fc53cfcf3c8406db8e126ffea
Lawal Adekunle Olusegun A <lawaladekunle@gmail.com>
2019-06-06 18:21:18 +0100

    Updated Readme, base.py and .gitignore

--- base.py
+++ base.py
@@ -9,3 +9,14 @@ if new_ >= 10:
 else:
 	print("This is not a base number.")
 
+
+!/usr/bin/python
+
+num1_ = str(input("Enter a number."))
+num2_ = str(input("Enter the second number."))
+num3_ = str(input("Enter the third number."))
+num4_ = str(input("Enter the fourth number."))
+
+multi_stat = float(num1_) * float(num2_) * float(num3_) * float(num4_)
+
+print("The pipy is -:", multi_stat)
