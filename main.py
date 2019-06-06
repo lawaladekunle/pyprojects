5391325a7984641135cd4f10a5c1111b08dd6cb9
Lawal Adekunle Olusegun A <lawaladekunle@gmail.com>
2019-06-06 16:18:31 +0100

    Adding the first python code.

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+print("Welcome to base by Lawal Adekunle :\). This is the first line of code in project py.")
+
+new_ = input("Enter a random number.")
+
+if new_ >= 10:
+	print("This is a base number.")
+else:
+	print("This is not a base number.")
+
